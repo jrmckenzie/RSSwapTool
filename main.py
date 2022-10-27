f7ad95be877849794e1654040fbb2f6785a91b47
jrm <jrmknz@yahoo.co.uk>
2022-10-27 20:30:47 +0100

    Add Armstrong Powerhouse TTA swapping / make version 1.0.5

--- main.py
+++ main.py
@@ -909,8 +909,7 @@ def tta_replace(provider, product, blueprint, name, number, loaded):
                     if number_suffix[0:1] == ';' and 'eTrue' in loaded.text:
                         # Wagon is loaded
                         # Change the blueprint and name to the unloaded wagon
-                        blueprint.text = re.sub('.xml', '_LD.xml', blueprint.text,
-                                                flags=re.IGNORECASE)
+                        blueprint.text = re.sub('.xml', '_LD.xml', blueprint.text, flags=re.IGNORECASE)
                         name.text = re.sub('Empty', 'Loaded', name.text, flags=re.IGNORECASE)
                     elif number_suffix[0:1] != ';':
                         number_suffix = number_suffix[1:]
